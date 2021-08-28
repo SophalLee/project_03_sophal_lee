0a89072e0165deee91d53d7552873295d9d0579f
Sophal Lee <sophal.lee@live.com>
2021-08-28 13:59:00 +1000

    Added comments

--- project_b/index.js
+++ project_b/index.js
@@ -3,26 +3,38 @@ const express = require('express');
 const crypto = require('crypto');
 const path =require('path');
 const app = express();
+const PORT = process.env.PORT || 3000
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-app.listen(3000);
 app.use(express.urlencoded({ extended: true }));
 
+app.listen(PORT, () => {
+    if(process.env.PORT) {
+        console.log('App is listening on port: ' + process.env.PORT);
+    }
+    else {
+        console.log('App is listening on port: ' + 3000);
+    }    
+});
 
+// Display the Welcome message
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+//Display all the users
 app.get('/users', (req, res) => {
     res.render('users', {users: users});
 });
 
+//Add a new user
 app.get('/users/new', (req, res) => {
     res.render('new-user');
 });
 
+//View a specific user
 app.get('/users/:id', (req, res, next) => {
 
     if(req.params.id < users.length) {
@@ -33,6 +45,7 @@ app.get('/users/:id', (req, res, next) => {
     }
 });
 
+// View a specific user's schedule
 app.get('/users/:id/schedules', (req, res) => {
     let obj = [];
     for( let i = 0; i < schedules.length; i++) {        
@@ -50,14 +63,17 @@ app.get('/users/:id/schedules', (req, res) => {
     
 });
 
+// View all schedules
 app.get('/schedules', (req, res) => {
     res.render('schedules', {schedules: schedules});
 });
 
+// Add a new schedule
 app.get('/schedules/new', (req, res) => {
     res.render('new-schedule', {users: users});
 });
 
+// Process the user data from the form
 app.post('/users', (req, res) => {
     const secret = 'secret key';
     let obj= { };
@@ -71,6 +87,7 @@ app.post('/users', (req, res) => {
     res.redirect('/users');
 });
 
+// Process the schedule data from the form
 app.post('/schedules', (req, res) => {
     let obj= { };
 
@@ -83,6 +100,7 @@ app.post('/schedules', (req, res) => {
     res.redirect('/schedules');
 });
 
+//Default 404 Error - Page not found
 app.use((req, res) => {
     res.status(404).send('404 Error - Page not found');
 });
