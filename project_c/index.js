0d8691f9225b53d6106f1e4af8320dc026ac7575
Sophal Lee <sophal.lee@live.com>
2021-09-01 14:17:57 +1000

    Changed code to use pg-promise instead of pg

--- project_c/index.js
+++ project_c/index.js
@@ -4,6 +4,7 @@ const path =require('path');
 const app = express();
 const { create } = require('domain');
 const { Pool } = require('pg');
+const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const publicDirectory = path.join(__dirname, './public');
@@ -16,42 +17,31 @@ app.set('view engine', 'pug');
 app.use(express.static(publicDirectory));
 
 
-
-app.listen(PORT, () => {
-    if(process.env.PORT) {
-        console.log('App is listening on port: ' + process.env.PORT);
-    }
-    else {
-        console.log('App is listening on port: ' + 3000);
-    }
-    
-});
-
 //Function to check the time entered is valid
 function validateInput(regex, input) {
     return regex.test(input);
 }
 
-const pool = new Pool({
+const connection = {
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT
-})
+}
+
+const db = pgp(connection);
 
 // Display schedule on home page
-app.get('/', (req, res) => {
-    const displaySchedule = `SELECT username, day, to_char(start_at, 'HH:MI AM') AS start_at, to_char(end_at, 'HH:MI AM') AS end_at FROM schedules`;
+app.get('/', (req, res) => { 
 
-    pool.query(displaySchedule, (err, results) => {
-        if(err) { 
-            throw err; 
-        }
-        else {
-            res.render('index', {schedules: results.rows});
-        }
-    })
+    db.any(`SELECT username, day, to_char(start_at, 'HH:MI AM') AS start_at, to_char(end_at, 'HH:MI AM') AS end_at FROM schedules`)
+        .then(schedules => {
+            res.render('index', {schedules});
+        })
+        .catch(err => {
+            throw err;
+        })
 });
 
 // //Form for adding a new schedule
@@ -62,21 +52,27 @@ app.get('/new', (req, res) => {
 //After posting the form, add the values into the schedules table
 app.post('/new', (req, res) => {
     const { username, day, start_at, end_at } = req.body;
-    const addSchedule = `INSERT INTO schedules (username, day, start_at, end_at) 
-                        VALUES('${username}', ${day}, '${start_at}', '${end_at}')`
 
     if(validateInput(validTime, req.body.start_at) && validateInput(validTime, req.body.end_at)) {
-        pool.query(addSchedule, (err) => {
-            if(err) throw err;
-        })
-        res.redirect('/new');
+        db.none('INSERT INTO schedules (username, day, start_at, end_at) VALUES($1, $2, $3, $4)', [ username, day, start_at, end_at])
+            .then(() => {
+                res.redirect('/');
+            })
+            .catch(err => {
+                throw err;
+            })        
     }
     else {
         res.send("Incorrect time format. Format is HH:MM<br><br><a href='/new'>Add a schedule</a>");
     }
 });
 
-
-
-
-
+app.listen(PORT, () => {
+    if(process.env.PORT) {
+        console.log('App is listening on port: ' + process.env.PORT);
+    }
+    else {
+        console.log('App is listening on port: ' + 3000);
+    }
+    
+});
